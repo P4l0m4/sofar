2ef952408fdb13ee9cfa0a3892c3aea9c7a4c50d
Paloma Tejeda <palomatejeda81@gmail.com>
2024-04-08 13:19:56 +0200

    why fly with us

--- src/components/home/WhyFlyWithSofar.vue
+++ src/components/home/WhyFlyWithSofar.vue
@@ -1,23 +1,28 @@
 <script setup lang="ts">
 const qualities = [
   {
+    img: "award_star",
     title: "Expert pilots",
     txt: "Expert pilots, passionate about precision and your travel experience.",
   },
   {
+    img: "health_and_safety",
     title: "Unrivalled reliability",
     txt: "Travel with confidence on our aircraft of unrivalled reliability, where safety and peace of mind define every flight.",
   },
   {
+    img: "flight_class",
     title: "Comfort and style",
     txt: "Fly in unparalleled comfort and style – comfort seats, curated refreshments, snacks, and WiFi at your fingertips. Elevate your travel experience.",
   },
 
   {
+    img: "straighten",
     title: "Tailored journey",
     txt: "From travel essentials to specialized gear, our flexible luggage policy ensures your journey is tailored to your passions and preferences.",
   },
   {
+    img: "connecting_airports_light",
     title: "Flexible schedules",
     txt: "Save valuable time and enjoy flexible schedules tailored to your needs and agenda.",
   },
@@ -35,45 +40,40 @@ const qualities = [
           class="why-fly-with-sofar__main-part__qualities__element"
           v-for="argument in qualities"
         >
-          <h3
-            class="why-fly-with-sofar__main-part__qualities__element__title subtitles"
-          >
-            {{ argument.title }}
-          </h3>
-          <p
-            class="why-fly-with-sofar__main-part__qualities__element__txt paragraphs"
-          >
-            {{ argument.txt }}
-          </p>
+          <span class="why-fly-with-sofar__main-part__qualities__element__img"
+            ><img :src="`/assets/icons/${argument.img}.svg`"
+          /></span>
+          <div class="why-fly-with-sofar__main-part__qualities__element__txt">
+            <h3
+              class="why-fly-with-sofar__main-part__qualities__element__txt__title subtitles"
+            >
+              {{ argument.title }}
+            </h3>
+            <p
+              class="why-fly-with-sofar__main-part__qualities__element__txt__paragraph paragraphs"
+            >
+              {{ argument.txt }}
+            </p>
+          </div>
         </div>
       </div>
     </div>
-    <div class="why-fly-with-sofar__gallery">
-      <img
-        class="why-fly-with-sofar__gallery__img"
-        src="@/assets/images/logo-dark.svg"
-        alt="private jet clouds background"
-      />
-      <img
-        class="why-fly-with-sofar__gallery__img"
-        src="@/assets/images/pilot.webp"
-        alt="private jet clouds background"
-      />
-      <img
-        class="why-fly-with-sofar__gallery__img"
-        src="@/assets/images/seats.webp"
-        alt="private jet clouds background"
-      />
-
-      <img
-        class="why-fly-with-sofar__gallery__img"
-        src="@/assets/images/seats2.webp"
-        alt="private jet clouds background"
-      />
-    </div>
+    <div class="masked-image"></div>
   </section>
 </template>
 <style lang="scss" scoped>
+.masked-image {
+  background-repeat: no-repeat;
+  -webkit-mask-image: url("@/assets/images/logo-light-vertical.svg");
+  mask-image: url("@/assets/images/logo-light-vertical.svg");
+  mask-size: contain;
+  mask-repeat: no-repeat;
+  background-color: $base-color;
+  width: 400px;
+  height: 100%;
+  transform: rotate(180deg);
+  opacity: 0.5;
+}
 .why-fly-with-sofar {
   display: flex;
   align-items: center;
@@ -83,6 +83,19 @@ const qualities = [
   padding: 1rem;
   height: 100svh;
   max-height: 800px;
+  background-image: url("@/assets/images/seats.webp");
+  background-size: cover;
+  background-position: center;
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    background-image: linear-gradient(80deg, $text-color, transparent 60%);
+  }
 
   @media (min-width: $big-tablet-screen) {
     gap: 4rem;
@@ -97,66 +110,54 @@ const qualities = [
     width: 100%;
     max-width: 760px;
     height: fit-content;
+    z-index: 1;
 
     @media (min-width: $big-tablet-screen) {
       gap: 2rem;
-      width: 60%;
+      width: 70%;
+    }
+    &__title {
+      width: fit-content;
+      white-space: nowrap;
+      color: $text-color-alt;
     }
 
     &__qualities {
       display: flex;
-      flex-direction: column;
       gap: 1rem;
       height: fit-content;
+      flex-direction: column;
 
       &__element {
         display: flex;
-        flex-direction: column;
         height: fit-content;
-        &__title {
-          width: fit-content;
-          white-space: nowrap;
+        align-items: center;
+        color: $text-color-alt;
+        gap: 1rem;
+
+        &__img {
+          width: 40px;
+          min-width: 40px;
+          height: 40px;
+
+          & img {
+            width: 100%;
+            height: 100%;
+          }
         }
+
         &__txt {
-          text-wrap: balance;
+          display: flex;
+          flex-direction: column;
+
+          &__title {
+            width: fit-content;
+          }
+
+          &__paragraph {
+            text-wrap: balance;
+          }
         }
-      }
-    }
-  }
-
-  &__gallery {
-    display: grid;
-    width: 100%;
-    grid-template-columns: repeat(2, 1fr);
-    gap: 1rem;
-    height: 100%;
-
-    @media (min-width: $big-tablet-screen) {
-      width: 40%;
-    }
-
-    &__img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-
-      &:nth-of-type(1) {
-        object-fit: contain;
-        grid-row: span 1;
-      }
-      &:nth-of-type(2) {
-        border-radius: calc($radius * 3) 0 calc($radius * 3) 0;
-        grid-row: span 2;
-        object-position: left;
-      }
-      &:nth-of-type(3) {
-        border-radius: calc($radius * 3) 0 calc($radius * 3) 0;
-        grid-row: span 3;
-      }
-
-      &:nth-of-type(4) {
-        border-radius: 0 calc($radius * 3) 0 calc($radius * 3);
-        grid-row: span 2;
       }
     }
   }
