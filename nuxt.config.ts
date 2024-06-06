6846a5c4a997c460ec1b5a1e3ed1c064bb548a4a
Paloma Tejeda <palomatejeda81@gmail.com>
2024-06-06 14:15:49 +0200

    gtm tag and bug fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -82,6 +82,16 @@ export default defineNuxtConfig({
         {
           src: "https://www.googletagmanager.com/gtag/js?id=G-KKF0WKPVWR",
         },
+        {
+          innerHTML: `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5004125,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+        },
       ],
     },
   },
